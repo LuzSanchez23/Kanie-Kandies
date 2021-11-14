f28ced95f38645344bc088d68a5f928421b53c94
Luz Sanchez <nena_baby23@hotmail.com>
2021-11-14 15:22:58 -0700

    correcting img

--- app.js
+++ app.js
@@ -46,45 +46,45 @@ console.log(meeting);
 
 
 
-        //Daily Specials starts here
-//         var day;
-// switch (new Date().getDay()) {
-//     case 0:
-//       day = "1/2 off all oils";
-//       break;
-//     case 1:
-//       day = "2-for-1 treats";
-//       break;
-//     case 2:
-//        day = "Buy-one-Get-one Free day";
-//       break;
-//     case 3:
-//       day = "Add a gummy";
-//       break;
-//     case 4:
-//       day = "Bring a friend get a extra treat";
-//       break;
-//     case 5:
-//       day = "Buy two edibles get one free";
-//       break;
-//     case 6:
-//       day = "`Brunch and free samples";
-//   }
-// document.getElementById("message1").innerHTML=(`⭐Today's  special⭐ </br> ${day}`);
-// console.log("Today us "+ day);
+//Daily Specials starts here
+        var day;
+switch (new Date().getDay()) {
+    case 0:
+      day = "1/2 off all oils";
+      break;
+    case 1:
+      day = "2-for-1 treats";
+      break;
+    case 2:
+       day = "Buy-one-Get-one Free day";
+      break;
+    case 3:
+      day = "Add a gummy";
+      break;
+    case 4:
+      day = "Bring a friend get a extra treat";
+      break;
+    case 5:
+      day = "Buy two edibles get one free";
+      break;
+    case 6:
+      day = "`Brunch and free samples";
+  }
+document.getElementById("message1").innerHTML=(`⭐Today's  special⭐ </br> ${day}`);
+console.log("Today us "+ day);
 //Daily Specials ends here
 
-// const arrayCarousel = [
-//   "images/img1.jpg",
-//   "images/img2.jpg",
-// ]
-// counter = 0
-// function setImage(){
-//   document.getElementById("carousel-image").src = arrayCarousel[counter];
-//   document.getElementById("carousel-image2").src = arrayCarousel[counter];
-//   counter= (counter+1) % arrayCarousel.length;
-// }
-// setInterval(setImage, 2000);
+const arrayCarousel = [
+  "images/pic1.png",
+  "images/pic2.png",
+];
+counter = 0
+function setImage(){
+  document.getElementById("carousel-image").src = arrayCarousel[counter];
+  document.getElementById("carousel-image2").src = arrayCarousel[counter];
+  counter= (counter+1) % arrayCarousel.length;
+}
+setInterval(setImage, 2000);
 
 
 // var choices = ['red', 'orange', 'pink', 'yellow'];
