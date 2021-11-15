48454c7aad1b92fae23ae40f980101fbbbc8ed95
Luz Sanchez <nena_baby23@hotmail.com>
2021-11-14 19:44:33 -0700

    working on main page

--- classlessons.js
+++ classlessons.js
@@ -138,3 +138,79 @@
     // }
     // getDonuts ();
     // Array Assignments endss here
+
+
+    //html cards
+//     <div class="row5">
+//     <img src="img/mole.jpg">
+//         alt="mole">
+//     <h3 id="title"></h3>
+//     <h3 id="servingSize"></h3>
+//     <h3 id="ingredients"></h3>
+// </div>
+//html cards ends here
+//
+//objects starts here   
+//object literall
+const recipe = {
+    title: "mole",
+    serving: 2,
+    ingredients: [' Cinnamon', ' Cumin', ' Cocoa']
+  };
+  // var food=(`how to make ` ${recipe}["name"]);
+  // console.log(`${recipe.title}`)
+  // console.log(`serving: ${recipe.serving}`)
+  // console.log(`Ingredients: `)
+  // for(var i=0; i<recipe.ingredients.length; i++) {
+  //   console.log(`${recipe.ingredients[i]}`);
+  // };
+//   document.getElementById('title').innerHTML = 'Title ' + recipe.title;
+//   document.getElementById('servingSize').innerHTML = 'Serving: ' + recipe.serving;
+//   document.getElementById('ingredients').innerHTML = 'Ingredients: ' + recipe.ingredients;
+  
+  
+  
+//   var sibling = {
+//     name: "Alex",
+//     age: 37,
+//     single: true,
+//     children: ["Ashlei", "Jr", "Alinna", "Dominik"],
+//     order: "older",
+//   };
+//   sibling.single = false;
+//   sibling.gender = "male";
+//   delete sibling.age;
+//   var meeting = (`Hello I am ${sibling["name"]} I am ${sibling["age"]} years old Am I single ${sibling["single"]}`);
+//   console.log(meeting);
+  
+  // document.getElementById("uno").innerHTML=sibling.name;
+  
+  //new keyword setting the values  
+  // var sibling=new Object();
+  // sibling.name="Alex";
+  // sibling.age=37;
+  // sibling.single= true;
+  // sibling.children=["Ashlei", "Jr", "Alinna", "Dominik"];
+  // sibling.order= "older";       
+  
+  
+  
+
+// var choices = ['red', 'orange', 'pink', 'yellow'];
+// for (var i = 0; i < choices.length; i++) {
+//     console.log('My #' + (i + 1) + ' choice is ' + choices[i]);
+// }
+// for (var i = 0; i < choices.length; i++) {
+//     var choiceNum = i + 1;
+//     var choiceNumSuffix;
+//     if (choiceNum == 1) {
+//         choiceNumSuffix = 'st';
+//     } else if (choiceNum == 2) {
+//         choiceNumSuffix = 'nd';
+//     } else if (choiceNum == 3) {
+//         choiceNumSuffix = 'rd';
+//     } else {
+//         choiceNumSuffix = 'th';
+//     }
+//     console.log('My ' + choiceNum + choiceNumSuffix + ' choice is ' + choices[i]);
+// }
