0ac96a64ed70fef4b6d56818ef2b4914f7352ecd
Luz Sanchez <nena_baby23@hotmail.com>
2021-11-28 21:52:42 -0700

    adding part 1 of final assigment

---
+++ final.js
@@ -0,0 +1,26 @@
+const seconds = document.getElementById('seconds');
+const minutes = document.getElementById('minutes');
+const hours= document.getElementById('hours');
+const days = document.getElementById('days');
+
+const container = document.getElementById('container');
+const currentYear = new Date ().getFullYear();
+
+const christmasDate = new Date (`December 25 ${currentYear} 00:00:00`);
+
+function updateContainer() {
+    const timeNow = new Date();
+    const result = christmasDate - timeNow;
+
+    const second = Math.floor (result / 1000) % 60;
+    const minute = Math.floor (result / 1000 / 60) % 60;
+    const hour = Math.floor (result / 1000 / 60 / 60) % 24;
+    const day = Math.floor (result / 1000 / 60 / 60 / 24);
+    
+    seconds.innerHTML = second;
+    minutes.innerHTML = minute;
+    hours.innerHTML = hour;
+    days.innerHTML = day;
+}
+
+setInterval(updateContainer, 1000);
